d0b53d02816036d9219ad93af74ca20696e0cd30
Callum McCann <cmccann@analytics8.com>
2020-07-17 14:54:05 +0000

    adding things

--- models/staging/stg_store.sql
+++ models/staging/stg_store.sql
@@ -1,6 +1,6 @@
 with store as (
 
-    select * from {{ ref('snapshot_store') }}
+    select * from {{ source('Sakila','store') }}
 
 )
 ,
@@ -11,10 +11,8 @@ final as (
         STORE_ID,
         MANAGER_STAFF_ID,
         ADDRESS_ID,
-        DBT_SCD_ID AS DBT_Incremental_ID,
-        DBT_UPDATED_AT
+        LAST_UPDATE
     FROM store
-    WHERE DBT_VALID_TO IS NULL
     
 )
 
