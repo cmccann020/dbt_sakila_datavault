8e497e087945195301a4b35699333ae3a1329165
Callum McCann <cmccann@analytics8.com>
2020-07-19 17:49:11 +0000

    gooo

--- models/staging/stg_language.sql
+++ models/staging/stg_language.sql
@@ -4,17 +4,25 @@ with language as (
 
 )
 ,
-
-,
-final as (
+clean_language as (
     SELECT
-        {{ dbt_utils.surrogate_key(['NAME']) }} AS LANGUAGE_HASH_KEY,
         LANGUAGE_ID,
         TRIM(NAME,'"') AS LANGUAGE_NAME,
         DBT_UPDATED_AT AS LOAD_DATE,
         DBT_VALID_FROM AS VALID_FROM,
         DBT_VALID_TO AS VALID_TO
     FROM language
+)
+,
+final as (
+    SELECT
+        {{ dbt_utils.surrogate_key(['LANGUAGE_NAME']) }} AS LANGUAGE_HASH_KEY,
+        LANGUAGE_ID,
+        LANGUAGE_NAME,
+        LOAD_DATE,
+        VALID_FROM,
+        VALID_TO
+    FROM clean_language
 )
 
 select * from final
